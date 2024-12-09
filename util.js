9e4971447a1c3754d5094e0e5353a0af3b21514f
Mathias Gomez <mathzsbs@proton.me>
2024-12-09 02:03:23 -0500

    chocolatada

--- util.js
+++ util.js
@@ -1,11 +1,14 @@
 const colors = require('colors/safe');
+const ms = require('./ms');
+
+ms(2, {})
 
 module.exports = {
   // COMO TE AMO CHATGPT CTM AAAAAA
   applyColors(text) {
     // Expresión regular para buscar el patrón {color} y {/color}
     return text.replace(
-      /\{(yellow|red|blue|white|green)\}(.*?)\{\/\1\}/g,
+      /\{(yellow|red|blue|white|green|magenta|gray|random|eval)\}(.*?)\{\/\1\}/g,
       (match, color, innerText) => {
         // Mapea el color a la función correspondiente en `colors`
         switch (color) {
@@ -19,6 +22,18 @@ module.exports = {
             return colors.white(innerText);
           case 'green':
             return colors.green(innerText);
+          case 'magenta':
+            return colors.magenta(innerText);
+          case 'gray':
+            return colors.gray(innerText);
+          case 'random':{
+            const colorKeys = ['red', 'blue', 'yellow', 'magenta', 'rainbow']
+            return colors[colorKeys[~~(Math.random()*colorKeys.length)]](innerText)
+          }
+          case 'eval': {
+            const evaled = eval(innerText)
+            return `${evaled}`
+          }
           default:
             return innerText; // Si no coincide, devuelve el texto sin cambios
         }
