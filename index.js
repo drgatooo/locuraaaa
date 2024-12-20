4ff9f7b35e430ad1066678fb0e47bfb9baa3fe30
Mathias Gomez <mathzsbs@proton.me>
2024-12-20 17:44:01 -0500

    muchos mensajes :v

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const fs = require('node:fs');
 const path = require('node:path');
 const { applyColors } = require('./util');
+const figlet = require('figlet');
 
 const PORT = process.env['PORT'] || 4321;
-const frames = loadFrames(path.join(__dirname, 'frames'));
 
 // files in frames/ should be named 0.txt, 1.txt, 2.txt, etc.
 
@@ -20,8 +20,25 @@ app.get('/papu', (_, res) => {
   res.send('<h1>:v</h1>');
 });
 
-app.get('/', (req, res) => {
+app.get('/list', (_, res) => {
+  const msgs = fs.readdirSync(path.join(__dirname, 'frames'));
+  res.send(
+    `\x1b[2J\x1b[3J\x1b[H\n${figlet.textSync('PAPUTV')}\nVideos (o algo así):\n${msgs.map(m => `- ${m}`).join('\n').replace(/\.txt/g, '')}\n\nUso: curl -L papu.fwrd.lol/<video>`
+  )
+});
+
+app.get('/', handleRequest);
+app.get('/:msg', handleRequest);
+
+/**
+ * Handle the request and send the frames to the client
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ * @returns 
+ */
+function handleRequest(req, res) {
   console.log(req.headers['user-agent']);
+  const msg = req.params.msg || 'papunavida';
 
   if (req.headers && req.headers['user-agent'] && !req.headers['user-agent'].includes('curl')) {
     res.writeHead(302, { Location: '/papu' });
@@ -30,16 +47,23 @@ app.get('/', (req, res) => {
   }
 
   let frameIndex = 0;
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  sendFrame(res, frameIndex);
-});
+  
+  try {
+    const frames = loadFrames(path.join(__dirname, 'frames', msg));
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    sendFrame(res, frames, frameIndex);
+  } catch (error) {
+    res.status(404).send('Try again with papu.fwrd.lol/=3');
+  }
+}
 
 /**
  * function to send frames sequentially to the server
  * @param {express.Response} res
+ * @param {string[]} frames
  * @param {number} frameIndex
  */
-function sendFrame(res, frameIndex) {
+function sendFrame(res, frames, frameIndex) {
   // clean the terminal
   res.write('\x1b[2J\x1b[3J\x1b[H');
 
@@ -55,7 +79,7 @@ function sendFrame(res, frameIndex) {
 
   setTimeout(
     () => {
-      sendFrame(res, frameIndex);
+      sendFrame(res, frames, frameIndex);
     },
     frame.startsWith('// withtext') ? 3000 : 1500
   );
